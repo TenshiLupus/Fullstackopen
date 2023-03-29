4238bfe5a8904488433dbe3229b2912754399ebb
Tenshilupus <Angel__CM@hotmail.com>
2023-03-29 17:25:38 +0200

    setState on form submission

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,25 +1,52 @@
 import { useState } from 'react'
 
+const Names = ({persons}) => {
+  return (
+    <>
+    <h2>Numbers</h2>
+    <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+    </ul>
+    </>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const personChangeHandler = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPersonHandler = (event) => {
+    event.preventDefault();
+    console.log(newName)
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson));
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPersonHandler}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={personChangeHandler}/>
+
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <Names persons={persons}/>
+      <div>debug: {newName}</div>
     </div>
+
   )
 }
 
