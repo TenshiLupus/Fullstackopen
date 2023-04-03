290e050fdbfe5159e1d7bf6ceac15909a8d8d46b
Tenshilupus <Angel__CM@hotmail.com>
2023-04-03 16:04:57 +0200

    progress on phonebook and notes

--- Part2/part2-notes/src/App.js
+++ Part2/part2-notes/src/App.js
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/note/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('') 
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+  
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    }
+  
+    const promise = axios.get('http://localhost:3001/notes')
+    promise.then(eventHandler)
+  }, [])
+  
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
--- Part2/part2-notes/src/index.js
+++ Part2/part2-notes/src/index.js
@@ -1,25 +1,10 @@
 import ReactDOM from "react-dom/client"
-
+import axios from 'axios'
 import App from "./App"
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-]
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-)
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
