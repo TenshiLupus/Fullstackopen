4638f8dec642f51452f0c3021528d465159908e4
Tenshilupus <Angel__CM@hotmail.com>
2023-04-05 08:16:26 +0200

    notes data persistance and promise chaining added

--- Part2/part2-notes/src/App.js
+++ Part2/part2-notes/src/App.js
@@ -1,36 +1,60 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+
 import Note from './components/note/Note'
+import noteService from '../service/notesService'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+
+  //When rerendering the component we want to retrieve all the store elements back into the browser
   useEffect(() => {
     console.log('effect')
-  
-    const eventHandler = response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
-    }
-  
-    const promise = axios.get('http://localhost:3001/notes')
-    promise.then(eventHandler)
+
+    noteService
+    .getAll()
+    .then(initialNotes => 
+      setNotes(initialNotes.data)
+      )
   }, [])
+
+  const toggleImportanceOf = id => {
+ 
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
   
   console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
+    
     const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+    content: newNote,
+    important: Math.random() < 0.5,
     }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -52,7 +76,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+          key={note.id} 
+          note={note} 
+          toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
