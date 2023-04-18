1572c98ffab9bcf7f8d703cfa83cd2e27b98660b
Tenshilupus <Angel__CM@hotmail.com>
2023-04-18 20:48:47 +0200

    react frontend added as static files and requested by the node backend on initiation

--- Part3/Npmsandbox/index.js
+++ Part3/Npmsandbox/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
   {
