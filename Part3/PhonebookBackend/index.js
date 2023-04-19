afa470bb16b9fb0bd118a13a0041f0eb87b8327e
Tenshilupus <Angel__CM@hotmail.com>
2023-04-19 16:09:00 +0200

    deployable phonebook version using fly

--- Part3/PhonebookBackend/index.js
+++ Part3/PhonebookBackend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (request,response) => {
     return JSON.stringify(request.body)
@@ -118,7 +121,7 @@ const unknownEndpoint = (request, response) => {
   
   app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server has been initiated and listening on port ${PORT}`)
 })
