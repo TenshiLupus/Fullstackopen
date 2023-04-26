bb31c3dfc9bc177709eda1c429ac4b812e379341
Tenshilupus <Angel__CM@hotmail.com>
2023-04-26 18:08:09 +0200

    Validation and ESlint Start

--- Part3/Npmsandbox/index.js
+++ Part3/Npmsandbox/index.js
@@ -1,68 +1,36 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
 const Note = require('./models/note')
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if(note){
-    response.json(note)
-  }
-  else{
-    response.status(404).end()
-  } 
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-const generateId = () => {
-  //Interesting way of spreading all the ids into array and then picked by the max function
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
+const requestLogger = (request, response, next) => {
+  console.log("Method", request.method);
+  console.log("Path", request.path);
+  console.log("Body", request.body);
+  console.log("---");
+  next()
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError'){
+      return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(express.json())
+app.use(cors())
+app.use(requestLogger)
+app.use(express.static('build'))
 
 app.post('/api/notes', (request, response) => {
 
@@ -74,16 +42,59 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId()
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note => {
+    if(note){
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/notes/:id', (request, response) => {
+  
+  Note.deleteOne({_id : request.params.id}).then(note => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
