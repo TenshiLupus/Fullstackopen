6bc748b8cce84fe6ce172c541af491e196cbe05c
Tenshilupus <Angel__CM@hotmail.com>
2023-04-29 21:39:27 +0200

    refactoring code into separate modules

--- Part3/Npmsandbox/models/note.js
+++ Part3/Npmsandbox/models/note.js
@@ -1,21 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
-    content: String,
+    content: {
+      type: String,
+      minLength: 5,
+      require: true,
+    },
     important: Boolean,
 })
 
