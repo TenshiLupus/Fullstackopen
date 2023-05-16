5f36b491ca9d10487f326dad00c29be26895cef6
Tenshilupus <Angel__CM@hotmail.com>
2023-05-16 21:46:46 +0200

    Frontend duplicated for part5

---
+++ Part5/Blog-Frontend/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client"
+import axios from 'axios'
+import App from "./App"
+
+
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
---
+++ Part5/Blog-Frontend/src/services/notes.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+const baseUrl = '/api/notes'
+
+//these functions will still return promises as response to other promises, "then" will return a promise from another "then" promise
+
+const getAll = () => {
+  
+  const requestPromise = axios.get(baseUrl);
+  const responseData = requestPromise.then(response => response.data);
+
+  return responseData
+}
+
+const create = newObject => {
+  
+  const requestPromise = axios.post(baseUrl, newObject);
+  const responseData = requestPromise.then(response => response.data);
+  return responseData;
+}
+
+const update = (id, newObject) => {
+  
+  const requestPromise = axios.put(`${baseUrl}/${id}`, newObject);
+  const responseData = requestPromise.then(response => response.data);
+  return responseData;
+}
+
+export default { getAll, create, update }
