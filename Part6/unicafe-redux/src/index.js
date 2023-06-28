7c082bb5542198b4c1c46bab386b6d295f85b1b4
Tenshilupus <Angel__CM@hotmail.com>
2023-06-28 13:33:54 +0200

    Unicafe redux test exercises done

---
+++ Part6/unicafe-redux/src/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+  }
+
+
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type: "OK"
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: "BAD"
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type:"ZERO"
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button> 
+      <button onClick={ok}>ok</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
